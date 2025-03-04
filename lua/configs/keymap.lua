564eb7d1336d00b944cbab3b60a801b0d5475095
Hugh He <plushugh@gmail.com>
2025-03-04 13:01:36 +0800

    update

--- lua/configs/keymap.lua
+++ lua/configs/keymap.lua
@@ -10,6 +10,8 @@ vim.keymap.set('n', '<leader>lQ', vim.diagnostic.setloclist, { desc = '[LSP] Ope
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('n', '<leader>q', '<cmd>q<CR>', { desc = 'quit' })
+
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Focus left window' })
@@ -18,7 +20,11 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Focus lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Focus upper window' })
 
 --  Resize with ALT+<Left/Right/Up/Down>
-vim.keymap.set('n', '<A-Left>', '<C-w><')
-vim.keymap.set('n', '<A-Right>', '<C-w>>')
-vim.keymap.set('n', '<A-Up>', '<C-w>+')
-vim.keymap.set('n', '<A-Down>', '<C-w>-')
+vim.keymap.set('n', '<M-H>', '<C-w><')
+vim.keymap.set('n', '<M-L>', '<C-w>>')
+vim.keymap.set('n', '<M-K>', '<C-w>+')
+vim.keymap.set('n', '<M-J>', '<C-w>-')
+
+-- Word wrap visual movement
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
